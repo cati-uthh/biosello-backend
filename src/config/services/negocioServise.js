2c0b4247e8e9c5945966bb222eb281b5f6c5c41d
Gabriel Gamez <gabo_pxndx4278@hotmail.com>
2026-06-23 01:08:58 -0600

    fix vercel blob private

--- src/config/services/negocioServise.js
+++ src/config/services/negocioServise.js
@@ -23,7 +23,7 @@ export const registrarNegocio = async (datosNegocio) => {
       
       // Subimos el archivo a Vercel Blob
       const blob = await put(`documentos/${Date.now()}-${nombreArchivo}`, buffer, {
-        access: 'public',
+        access: 'restricted',
       });
       
       // Vercel nos devuelve la URL pública y segura
